e2abed192a3a1e5b50ce6a9d767062656340d2ff
shila atashzar <atashzar27.shila@gmail.com>
2023-06-22 08:10:56 +0330

    update project

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -1,46 +1,12 @@
-'use client';
-import { Container, Divider, Stack } from '@mui/material';
-import { colorPalette } from '@uiKits/colors';
-import Button from '@uiKits/button/Button';
-import ToggleButton from '@uiKits/toggleButton/ToggleButton';
-import Filters from './Filters';
-import Table from './Table';
 
-const Items = () => {
+import Items from '@/container/items';
+
+const ItemsPagee = () => {
 
     return (
-
-        <Stack
-            minHeight={'100vh'}
-            bgcolor={colorPalette.white}
-        >
-            <Container maxWidth='lg'>
-                <Stack
-                    flexDirection={'row'}
-                    justifyContent={'space-between'}
-                    p={'25px 0'}
-                >
-                    <ToggleButton leftName='Sets' rightName='Lights' onChange={(val: string) => console.log(val)} />
-                    <Button
-                        type='button'
-                        variant='contained'
-                        backgroundColor={colorPalette.yellow}
-                        LabelColor={colorPalette.black}
-                    >
-                        Create set
-                    </Button>
-
-                </Stack>
-                <Divider sx={{ borderColor: colorPalette.black }} />
-                <Filters />
-                <Divider sx={{ borderColor: colorPalette.black }} />
-                <Stack>
-                    <Table />
-                </Stack>
-            </Container>
-        </Stack>
+        <Items />
     )
 }
 
-export default Items;
+export default ItemsPagee;
 
